e6265a3f19fa93f8eb7625add1889c167647326a
Jonathan Estrella <jestrella04@gmail.com>
2016-09-23 17:53:31 -0400

    Basic functionality added

--- app/_/inc/view/home/main.php
+++ app/_/inc/view/home/main.php
@@ -1,29 +1,36 @@
 <div class="row">
-    <div class="col-md-6">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <div class="pull-right">
-                    <i class="fa fa-exclamation-triangle" aria-hidden="true"></i>
-                </div>
-                <h1 class="panel-title">Recent Issues</h1>
-            </div>
-            <div class="panel-body">
-                Panel content
-            </div>
+  <div class="col-md-6">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <div class="pull-right">
+          <i class="fa fa-exclamation-triangle" aria-hidden="true"></i>
         </div>
+        <h1 class="panel-title">Recent Issues</h1>
+      </div>
+      <div class="panel-body">
+        <div class="row">
+          <div class="col-md-2">Project</div>
+          <div class="col-md-2">Status</div>
+          <div class="col-md-8">Title [Link]</div>
+        </div>
+      </div>
     </div>
+  </div>
 
-    <div class="col-md-6">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <div class="pull-right">
-                    <i class="fa fa-clock-o" aria-hidden="true"></i>
-                </div>
-                <h1 class="panel-title">Recent Activity</h1>
-            </div>
-            <div class="panel-body">
-                Panel content
-            </div>
+  <div class="col-md-6">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <div class="pull-right">
+          <i class="fa fa-clock-o" aria-hidden="true"></i>
         </div>
+        <h1 class="panel-title">Recent Activity</h1>
+      </div>
+      <div class="panel-body">
+        <div class="row">
+          <div class="col-md-2">Date</div>
+          <div class="col-md-8">User action on Title [Link]</div>
+        </div>
+      </div>
     </div>
+  </div>
 </div>
